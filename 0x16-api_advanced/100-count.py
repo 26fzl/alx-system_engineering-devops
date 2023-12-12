fcc10ad573ca9fa4471aa3467f6804dccaa42c8b
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-12-12 08:19:06 -0800

    tasks

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -45,9 +45,9 @@ def count_words(subreddit, word_list, word_count={}, after=None):
                     word_count[word] += 1
 
     if not info.get("data").get("after"):
-        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
         sorted_counts = sorted(word_count.items(),
-                               key=lambda kv: kv[1], reverse=True)
+                               key=lambda kv: (kv[1], kv[0].lower()),
+                               reverse=True)
         [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
     else:
         return count_words(subreddit, word_list, word_count,
